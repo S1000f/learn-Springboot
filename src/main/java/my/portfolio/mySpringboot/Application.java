63a145bc4f2cfda1a0e82b757ef0238331fe725e
S1000f <dohp.kim@gmail.com>
2020-03-15 00:13:04 +0900

    make a testcode

---
+++ src/main/java/my/portfolio/mySpringboot/Application.java
@@ -0,0 +1,11 @@
+package my.portfolio.mySpringboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        run(Application.class, args);
+    }
+}
