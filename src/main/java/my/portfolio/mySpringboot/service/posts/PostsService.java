6de4b3bd9fc1f4c7561d37b134bb08afdef659c9
S1000f <dohp.kim@gmail.com>
2020-03-19 20:29:01 +0900

    add update API and test via h2 db

--- src/main/java/my/portfolio/mySpringboot/service/posts/PostsService.java
+++ src/main/java/my/portfolio/mySpringboot/service/posts/PostsService.java
@@ -1,8 +1,11 @@
 package my.portfolio.mySpringboot.service.posts;
 
 import lombok.RequiredArgsConstructor;
+import my.portfolio.mySpringboot.domain.posts.Posts;
 import my.portfolio.mySpringboot.domain.posts.PostsRepository;
+import my.portfolio.mySpringboot.web.Dto.PostsResponseDto;
 import my.portfolio.mySpringboot.web.Dto.PostsSaveRequestDto;
+import my.portfolio.mySpringboot.web.Dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,5 +19,22 @@ public class PostsService {
     @Transactional
     public long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                                     .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                                      .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        return new PostsResponseDto(entity);
     }
 }
---
+++ src/main/java/my/portfolio/mySpringboot/web/Dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package my.portfolio.mySpringboot.web.Dto;
+
+import lombok.Getter;
+import my.portfolio.mySpringboot.domain.posts.Posts;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
