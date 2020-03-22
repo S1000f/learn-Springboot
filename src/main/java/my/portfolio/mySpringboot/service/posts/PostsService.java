3415ed8561ca0a06ddd0fcb0c834a9f64adeebc1
S1000f <dohp.kim@gmail.com>
2020-03-22 14:37:13 +0900

    add article list,update,delete on View and service layer

--- src/main/java/my/portfolio/mySpringboot/service/posts/PostsService.java
+++ src/main/java/my/portfolio/mySpringboot/service/posts/PostsService.java
@@ -3,11 +3,15 @@ package my.portfolio.mySpringboot.service.posts;
 import lombok.RequiredArgsConstructor;
 import my.portfolio.mySpringboot.domain.posts.Posts;
 import my.portfolio.mySpringboot.domain.posts.PostsRepository;
+import my.portfolio.mySpringboot.web.Dto.PostsListResponseDto;
 import my.portfolio.mySpringboot.web.Dto.PostsResponseDto;
 import my.portfolio.mySpringboot.web.Dto.PostsSaveRequestDto;
 import my.portfolio.mySpringboot.web.Dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -31,10 +35,25 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다 id="+id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                                       .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                              .stream()
+                              .map(PostsListResponseDto::new)
+                              .collect(Collectors.toList());
     }
 }
--- src/main/java/my/portfolio/mySpringboot/web/IndexController.java
+++ src/main/java/my/portfolio/mySpringboot/web/IndexController.java
@@ -1,18 +1,36 @@
 package my.portfolio.mySpringboot.web;
 
+import lombok.RequiredArgsConstructor;
+import my.portfolio.mySpringboot.service.posts.PostsService;
+import my.portfolio.mySpringboot.web.Dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
+    private final PostsService postsService;
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        return "index";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
