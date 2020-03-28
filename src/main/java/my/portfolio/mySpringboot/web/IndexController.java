76445f63d483c55db5e2c653bbfbf4fc2a9968e1
dohpkim <dohp.kim@gmail.com>
2020-03-28 17:53:55 +0900

    add Google oauth login

--- src/main/java/my/portfolio/mySpringboot/web/IndexController.java
+++ src/main/java/my/portfolio/mySpringboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package my.portfolio.mySpringboot.web;
 
 import lombok.RequiredArgsConstructor;
+import my.portfolio.mySpringboot.config.auth.dto.SessionUser;
 import my.portfolio.mySpringboot.service.posts.PostsService;
 import my.portfolio.mySpringboot.web.Dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,11 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/posts/save")
     public String postsSave() {
@@ -22,6 +26,10 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
